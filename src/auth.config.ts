87d529bc834ebe377fbd37150525f58041fce07d
Mo'men <mmm.isam.99@gmail.com>
2025-11-06 11:41:45 +0200

    deploing try three

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -6,8 +6,8 @@ import type { NextAuthConfig } from "next-auth";
 export default {
   providers: [
     Google({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      clientId: process.env.AUTH_GOOGLE_ID,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET,
     }),
   ],
 } satisfies NextAuthConfig;
